cb99a9184ac65fe29630b17da95811fd3bf3ce7b
Ritanshu Tiwari <ritanshutiwari593@gmail.com>
2022-11-21 09:42:46 +0530

    fibonacci Number

---
+++ fibonacci.java
@@ -0,0 +1,10 @@
+public class fibonacci{
+      public static void main(String[]args) {
+        System.out.println(fibo(7));
+        
+      }
+      public static  int fibo(int n){
+            if(n<2) return n;//base condition
+            return fibo(n-1)+fibo(n-2);
+      }
+}
